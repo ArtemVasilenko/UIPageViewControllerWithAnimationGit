d0e0237ef37c325fc2765a1fc971bdb0817130e2
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-16 19:05:46 +0300

    Initial Commit

---
+++ UIPageViewControllerWithAnimationGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageViewControllerWithAnimationGit
+//
+//  Created by Артем on 4/16/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
